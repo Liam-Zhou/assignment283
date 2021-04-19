8710b4d5e4f68130bcea51bfbca23ce0e06213f9
YilinZhou <zhouyilinbest@gmail.com>
2021-04-19 12:37:56 -0700

    data strcture

--- assignment-1/cmpe283-1.c
+++ assignment-1/cmpe283-1.c
@@ -12,6 +12,10 @@
  * See SDM volume 4, section 2.1
  */
 #define IA32_VMX_PINBASED_CTLS	0x481
+#define IA32_VMX_PROCBASED_CTLS	0x482
+#define IA32_VMX_PROCBASED_CTLS2 0x48B
+#define IA32_VMX_EXIT_CTLS	0x483
+#define IA32_VMX_ENTRY_CTLS	0x484
 
 /*
  * struct caapability_info
@@ -36,6 +40,108 @@ struct capability_info pinbased[5] =
 	{ 5, "Virtual NMIs" },
 	{ 6, "Activate VMX Preemption Timer" },
 	{ 7, "Process Posted Interrupts" }
+};
+
+/*
+* Processor-Based VM-Execution Controls
+*/
+struct capability_info procbased[21] =
+{
+	{2 , "Interrupt-window" },
+	{3 , "Use TSC offsetting" },
+	{7 , "HLT exiting" },
+	{9 , "INVLPG exiting" },
+	{10 , "MWAIT exiting" },
+	{11 , "RDPMC exiting" },
+	{12 , "RDTSC exiting" },
+	{15 , "CR3-load exiting" },
+	{16 , "CR3-store exiting" },
+	{19 , "CR8-load exiting" },
+	{20 , "CR8-store exiting" },
+	{21 , "Use TPR shadow" },
+	{22 , "NMI-window exiting" },
+	{23 , "MOV-DR exiting" },
+	{24 , "Unconditional I/O" },
+	{25 , "Use I/O bitmaps" },
+	{27 , "Monitor trap flag" },
+	{28 , "Use MSR Bitmaps" },
+	{29 , "MONITOR exiting" },
+	{30 , "PAUSE exiting" },
+	{31 , "Activate secondary controls" }
+};
+
+/*
+* Secondary Processor-Based VM-Execution Controls
+*/
+struct capability_info procbased_2[27] =
+{
+	{ 0, "Virtualize APIC accesses" },
+	{ 1, "Enable EPT" },
+	{ 2, "Descriptor-table exiting" },
+	{ 3, "Enable RDTSCP" },
+	{ 4, "Virtualize x2APIC mode" },
+	{ 5, "Enable VPID" },
+	{ 6, "WBINVD exiting" },
+	{ 7, "Unrestricted guest" },
+	{ 8, "APIC-register virtualization" },
+	{ 9, "Virtual-interrupt delivery" },
+	{ 10, "PAUSE-loop exiting" },
+	{ 11, "RDRAND exiting" },
+	{ 12, "Enable INVPCID" },
+	{ 13, "Enable VM functions" },
+	{ 14, "VMCS shadowing" },
+	{ 15, "Enable ENCLS exiting" },
+	{ 16, "RDSEED exiting" },
+	{ 17, "Enable PML" },
+	{ 18, "EPT-violation" },
+	{ 19, "Conceal VMX nonroot operation from Intel PT" },
+	{ 20, "Enable XSAVES/XRSTORS" },
+	{ 22, "Mode-based execute control for EPT" },
+	{ 23, "Sub-page write permissions for EPT"},
+	{ 24, "Intel PT uses guest physical addresses"},
+	{ 25, "Use TSC scaling" },
+	{ 26, "Enable user wait and pause"},
+	{ 28, "Enable ENCLV exiting"}
+};
+
+/*
+ * VM Exit Control capabilities
+ */
+struct capability_info vm_exit_controls[14] =
+{
+	{ 2, "Save debug controls" },
+	{ 9, "Host address- space size" },
+	{ 12, "Load IA32_PERF_GLOB AL_CTRL" },
+	{ 15, "Acknowledge interrupt on exit" },
+	{ 18, "Save IA32_PAT" },
+	{ 19, "Load IA32_PAT" },
+	{ 20, "Save IA32_EFER" },
+	{ 21, "Load IA32_EFER" },
+	{ 22, "Save VMX-preemption timer value" },
+	{ 23, "Clear IA32_BNDCFGS" },
+	{ 24, "Conceal VM exits from Intel PT" },
+	{ 25, "Clear IA32_RTIT_CTL"},
+	{ 28, "Load CET state"},
+	{ 29, "Load PKRS"}
+};
+
+/*
+ * VM Entry Control capabilities
+ */
+struct capability_info vm_entry_controls[12] =
+{
+	{ 2, "Load debug controls" },
+	{ 9, "IA-32e mode guest" },
+	{ 10, "Entry to SMM" },
+	{ 11, "Deactivate dual-monitor treatment" },
+	{ 13, "Load IA32_PERF_GLOBAL_CTRL" },
+	{ 14, "Load IA32_PAT" },
+	{ 15, "Load IA32_EFER" },
+	{ 16, "Load IA32_BNDCFGS" },
+	{ 17, "Conceal VM entries from Intel PT" },
+	{ 18, "Load IA32_RTIT_CTL"},
+	{ 20, "Load CET state"},
+	{ 22, "Load PKRS"}
 };
 
 /*
